a6082641e88088b2e96a0b6f03bc7a19d39297e4
mc-perry <jonathan@m-craft.com>
2020-07-20 16:32:11 +0900

    confirmed completion of 特別課題 1

--- kadai_tsuika/app/Repositories/PartitionBatchTestRepository.php
+++ kadai_tsuika/app/Repositories/PartitionBatchTestRepository.php
@@ -18,18 +18,16 @@ class PartitionBatchTestRepository
      */
     public function partition()
     {
-        // return UserModel::query()->where('id', $UserId)->first();
         $beginOfDay = strtotime("today");
         $dayAfterTomorrow   = strtotime('tomorrow + 1 day');
 
         $partitionQuery = "alter table jonathan_batch_test
-        PARTITION BY RANGE(UNIX_TIMESTAMP (created_at) )
-        ( PARTITION part_less_than_today VALUES LESS THAN (" . $beginOfDay . "),
+        PARTITION BY RANGE(UNIX_TIMESTAMP (created_at) ) (
+            PARTITION part_less_than_today VALUES LESS THAN (" . $beginOfDay . "),
             PARTITION part_within_twodays VALUES LESS THAN (" . $dayAfterTomorrow . "),
-            PARTITION part_after_twodays VALUES LESS THAN MAXVALUE
+            PARTITION part_after_twodays VALUES LESS THAN (MAXVALUE)
         );";
 
-        var_dump($partitionQuery);
         DB::unprepared(DB::raw($partitionQuery));
     }
 }
